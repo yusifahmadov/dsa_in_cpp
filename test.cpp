2b8d344f59fc168be47486adf4e62f4d40b08bd3
Yusif Ahmadov <yusifakhmad@gmail.com>
2025-01-08 19:30:39 +0100

    initial

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   
+    int x = 10;
+
+    int y = ++x;
+    
+    
+    
+
+    cout << y << endl;
+
+
+    return 0;
+}
