008ea62ebe6ef6df65644564783487621486aec0
Yusif Ahmadov <yusifakhmad@gmail.com>
2025-01-09 19:37:34 +0100

    Functions added

--- cpp_concepts/Functions/functions.cpp
+++ cpp_concepts/Functions/functions.cpp
@@ -1,5 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool saveEarth(bool vSaveEarth){
+
+    bool saveEarth = true;
+
+    if(saveEarth == true && vSaveEarth == true){
+        cout << "Save the Earth" <<endl;
+    }else{
+        cout << "Do not need to save" << endl;
+    }
+    return saveEarth;
+
+}
+
+int add (int a, int b) { // Here x and y are the formal parameters
+    int c;
+    c = a+b;
+
+    return c;
+}
+
+
+
+
 int main(int argc, char const *argv[])
-{
-    /* code */
+{   
+    int x,y,z;
+
+    x =10;
+    y= 5;
+
+    z = add (x,y); // Here x and y are the actual parameters
+
+    cout << z << endl;
+
+   bool savingTheEarth = saveEarth(true);
+
+   cout << savingTheEarth << endl;
     return 0;
 }
