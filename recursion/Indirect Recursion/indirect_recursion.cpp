f3a8485f9d61d3379c9e89255e817f117ef41896
Yusif Ahmadov <yusifakhmad@gmail.com>
2025-01-10 20:11:41 +0100

    Indirect recursion added

---
+++ recursion/Indirect Recursion/indirect_recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void funB(int n);
+
+void funA(int n){
+    if ( n >0){
+        cout << n << endl;
+        funB(n-1);
+    }
+}
+
+void funB(int n){
+    if(n>1){
+        cout << n << endl;
+        funA(n/2);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    funA(20);
+    return 0;
+}
