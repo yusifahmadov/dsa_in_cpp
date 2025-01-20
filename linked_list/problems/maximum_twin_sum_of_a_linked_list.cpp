30527ce0baac727d1d751b664d22c44d46b2627e
Yusif Ahmadov <yusifakhmad@gmail.com>
2025-01-20 14:03:09 +0100

    Maximum twin summ problem added

---
+++ linked_list/problems/maximum_twin_sum_of_a_linked_list.cpp
@@ -0,0 +1,56 @@
+#include "../linked_list.cpp"
+
+#include "../node.cpp"
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    int pairSum(Node *head)
+    {
+        vector<int> twinSums1;
+
+        int maxSum = 0;
+        int n = 0;
+
+        Node *temp = head;
+
+        while (temp)
+        {
+            twinSums1.push_back(temp->value);
+            temp = temp->next;
+        }
+        vector<int> twinSums2 = twinSums1;
+        reverse(twinSums2.begin(), twinSums2.end());
+
+        for (int i = 0; i < twinSums1.size(); i++)
+        {
+
+            if (maxSum < twinSums1[i] + twinSums2[i])
+            {
+                maxSum = twinSums1[i] + twinSums2[i];
+            }
+        }
+
+        return twinSums2[0] + twinSums1[0];
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+
+    LinkedList *linkedList = new LinkedList(5);
+    Solution *solution = new Solution();
+    linkedList->append(4);
+    linkedList->append(2);
+    linkedList->append(1);
+
+    int result = solution->pairSum(linkedList->getHead());
+
+    cout << result << endl;
+
+    linkedList->printList();
+
+    return 0;
+}
